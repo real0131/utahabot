2ad1aec54585dfda97906471688a5a5a34823cdb
Jaeho Cho <real0131@gmail.com>
2016-09-22 15:41:31 +0900

    ver 0.1

--- bot.py
+++ bot.py
@@ -2,6 +2,31 @@ import urllib.request import urlopen
 from bs4 import Beautifulsoup
 import schedule
 import time
-import twitter import *
+from twitter import *
+
+t = Twitter(auth=OAuth('',
+                       '',
+                       '',
+                       ''))
 
 def bot():
+    html  = urlopen("http://utaha.moe")
+    moe = Beautifulsoup(html)
+    header = moe.find('head').findAll('meta',{'name' : 'description'})
+    now = time.localtime()
+
+    s = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+
+    tweets = ""
+
+    tweets += str(s) + "\n" + "현재 utaha.moe는?" + "\n"
+    if not header:
+        tweets += "\n" + "퍼퍼펑"
+    else :
+        tweets += "\n" + "정상작동중"
+    tweets += "\n" + "http://utaha.moe"
+
+schedule.every(30).minutes.do(bot)
+
+while True:
+    schedule.run_pending()
