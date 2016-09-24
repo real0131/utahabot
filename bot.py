31db4397c2c331745eaf5149065123af0241b91e
Jaeho Cho <real0131@gmail.com>
2016-09-24 23:30:10 +0900

    fix import

--- bot.py
+++ bot.py
@@ -1,17 +1,17 @@
-import urllib.request import urlopen
-from bs4 import Beautifulsoup
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 import schedule
 import time
 from twitter import *
 
-t = Twitter(auth=OAuth('',
-                       '',
-                       '',
-                       ''))
+t = Twitter(auth=OAuth('778842210317979648-8sLyyVyQX1iS3FD1euWbrU0GoR7KB5q',
+                       'x1wmaZhler7l5uYpaXyATNLGBOs2hFuYLOVBhEcrI4yj8',
+                       'dKi9X6qmkhzsbNr0NWJz5ch9J',
+                       '4EF4rzZDuuUPToCxEaeUMQehff3hciglmqZ1WEY1tBgAKr8Lb2'))
 
 def bot():
     html  = urlopen("http://utaha.moe")
-    moe = Beautifulsoup(html)
+    moe = BeautifulSoup(html)
     header = moe.find('head').findAll('meta',{'name' : 'description'})
     now = time.localtime()
 
@@ -25,8 +25,9 @@ def bot():
     else :
         tweets += "\n" + "정상작동중"
     tweets += "\n" + "http://utaha.moe"
+    t.statuses.update(status=tweets)
 
-schedule.every(30).minutes.do(bot)
+schedule.every(1).minutes.do(bot)
 
 while True:
     schedule.run_pending()
