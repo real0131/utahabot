0ecc38c8ed4566a82ce797cfd77f0bfbe27cee03
Jaeho Cho <real0131@gmail.com>
2016-09-26 01:06:24 +0900

    change tweet

--- bot.py
+++ bot.py
@@ -10,22 +10,29 @@ t = Twitter(auth=OAuth('Access Token',
                        'Access Token Secret',
                        'Consumer Key',
                        'Consumer Key Secret'))
+isServer = True
 
-def bot():
-    html  = urlopen("http://utaha.moe")
+def bot(isServer):
+    html = urlopen("http://utaha.moe")
     moe = BeautifulSoup(html)
-    header = moe.find('head').findAll('meta',{'name' : 'description'})
-    now = time.localtime()
-
-    s = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
-
-    tweets = ""
-
-    tweets += str(s) + "\n" + "현재 utaha.moe는?" + "\n"
+    header = moe.find('head').findAll('meta', {'name': 'description'})
     if not header:
-        tweets += "\n" + "퍼퍼펑"
-    else :
-        tweets += "\n" + "정상작동중"
+        if isServer is True:
+            tweet()
+            isServer = False
+        else:
+            return 0
+    else:
+        if isServer is False:
+            isServer = True
+        return 0
+
+def tweet():
+    now = time.localtime()
+    s = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+    tweets = ""
+    tweets += str(s) + "\n" + "현재 utaha.moe는?" + "\n"
+    tweets += "\n" + "퍼퍼펑"
     tweets += "\n" + "http://utaha.moe"
     t.statuses.update(status=tweets)
 
